31f9a0166a1fd338854b86e85d8f851750bef7ae
admin <565959480@qq.com>
2016-11-10 14:28:43 +0800

    测试无

--- xuimanage/src/com/xui/action/IndexAction.java
+++ xuimanage/src/com/xui/action/IndexAction.java
@@ -29,7 +29,7 @@ public class IndexAction extends ActionSupport {
 		LogUtils.log("key:"+key);
 		LogUtils.log("nowKey:"+mdKey);
 		if(StringUtils.isEmpty(key)){
-			this.addFieldError("errorMessage", "测试三，你大爷的又来了！。这里什么都没有(⊙o⊙)哦！");
+			this.addFieldError("errorMessage", "测试五，Hello World！。这里什么都没有(⊙o⊙)哦！");
 			return ERROR;
 		}
 		if(key.equals(mdKey)){
